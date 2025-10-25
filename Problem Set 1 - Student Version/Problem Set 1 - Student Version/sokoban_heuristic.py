e7a26f6632c36297193b8894810bb991e4e4a7b0
Fatmaz04 <224542953+fatmaZenhom@users.noreply.github.com>
2025-10-25 13:04:39 +0300

    h(n) start: wait till Astar

--- Problem Set 1 - Student Version/Problem Set 1 - Student Version/sokoban_heuristic.py
+++ Problem Set 1 - Student Version/Problem Set 1 - Student Version/sokoban_heuristic.py
@@ -17,4 +17,5 @@ def strong_heuristic(problem: SokobanProblem, state: SokobanState) -> float:
     # which is the number of get_actions calls during the search
     #NOTE: you can use problem.cache() to get a dictionary in which you can store information that will persist between calls of this function
     # This could be useful if you want to store the results heavy computations that can be cached and used across multiple calls of this function
-    NotImplemented()
+    if problem.is_goal(state): return 0
+    return min(manhattan_distance(state.player, crate) for crate in state.crates)
