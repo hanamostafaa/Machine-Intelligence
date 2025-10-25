948f6e00cc1d10157d1c282a07f2852c03bb8e40
Fatmaz04 <224542953+fatmaZenhom@users.noreply.github.com>
2025-10-25 12:14:52 +0300

    BFS P2

--- Problem Set 1 - Student Version/Problem Set 1 - Student Version/search.py
+++ Problem Set 1 - Student Version/Problem Set 1 - Student Version/search.py
@@ -15,7 +15,23 @@ import heapq
 
 def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     #TODO: ADD YOUR CODE HERE
-    NotImplemented()
+    if problem.is_goal(initial_state): # check if already at goal no actions needed
+        return []
+    
+    q = deque([(initial_state, [])]) # queue for BFS
+    visited = set([initial_state]) # keeping track of visited nodes to avoid cycles
+
+    while q:
+        state, path = q.popleft()
+        for action in problem.get_actions(state): # getting every action possible at current state
+            successor = problem.get_successor(state, action) # getting new state from that action 
+            if successor not in visited:  # if not visited
+                visited.add(successor) # mark as visited
+                new_path = path + [action] # add action to path
+                if problem.is_goal(successor): # if at goal return path 
+                    return new_path
+                q.append((successor, new_path)) # else add to queue and continue searching 
+    return None # no path found return None 
 
 def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     #TODO: ADD YOUR CODE HERE
