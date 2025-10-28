1c68c298dd02a8d6aafeb6fb3d81e6947df6121f
hanamostafaa <hmostafa935@gmail.com>
2025-10-28 13:40:01 +0300

    Another approach for sokoban heuristic

--- Problem Set 1 - Student Version/sokoban_heuristic.py
+++ Problem Set 1 - Student Version/sokoban_heuristic.py
@@ -18,12 +18,50 @@ def strong_heuristic(problem: SokobanProblem, state: SokobanState) -> float:
 
     if not crates:
         return 0
+    
+    remaining_goals = set(goals)
+    total_cost = 0
+    crates_with_min_dist = []
+    for crate in crates:
+        if crate in goals:
+            min_dist = 0
+            min_goal = crate
+        else:
+            min_dist = float('inf')
+            min_goal = None
+            for goal in goals:
+                dist = manhattan_distance(goal, crate)
+                if dist < min_dist:
+                    min_dist = dist
+                    min_goal = goal
+        crates_with_min_dist.append((crate, min_dist, min_goal))
 
-    # Sum of each crate's distance to nearest goal
-    total_distance = 0
+    crates_with_min_dist.sort(key=lambda x: x[1])
+    
+    # Greedily assign boxes to targets
+    for crate, _, _ in crates_with_min_dist:
+        if crate in remaining_goals:
+            remaining_goals.remove(crate)
+            continue
+            
+        # Find the closest remaining target
+        min_dist = float('inf')
+        closest_goal = None
+        
+        for goal in remaining_goals:
+            dist = manhattan_distance(crate, goal)
+            if dist < min_dist:
+                min_dist = dist
+                closest_goal = goal
+        
+        if closest_goal:
+            total_cost += min_dist
+            remaining_goals.remove(closest_goal)
+        else:
+            # If no remaining targets, add minimum distance to any target
+            # (this is a simplification - should only happen with more boxes than targets)
+            min_dist = min(manhattan_distance(crate,t) for t in goals)
+            total_cost += min_dist
     min_player_dist = min(manhattan_distance(state.player, crate) for crate in state.crates) - 1
-    misplaced_crates = [crate for crate in crates if crate not in goals]
-    for crate in misplaced_crates:
-        min_dist = min(manhattan_distance(crate, goal) for goal in goals)
-        total_distance += min_dist
-    return total_distance + min_player_dist
+    total_cost = total_cost / len(crates)
+    return round(0.67 * total_cost + 0.3* min_player_dist,1)
